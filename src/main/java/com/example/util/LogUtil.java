aa5b93d4149b63ca33a7c145dad6468679a58548
Purbita Mitra <purbita.mitra01@gmail.com>
2024-06-10 15:21:37 +0530

    util

---
+++ src/main/java/com/example/util/LogUtil.java
@@ -0,0 +1,12 @@
+package com.example.util;
+
+public class LogUtil {
+
+    private LogUtil() {
+    }
+
+    public static String print(String log) {
+        System.out.println(log);
+        return "done";
+    }
+}
